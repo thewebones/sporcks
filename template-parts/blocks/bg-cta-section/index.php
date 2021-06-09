f339e9cdc55f0165692ca4cfc531f54f1840a349
alejandro94020423284 <alejandro.pelaez940204@gmail.com>
2021-06-09 01:01:21 -0400

    cambios menu

--- template-parts/blocks/bg-cta-section/index.php
+++ template-parts/blocks/bg-cta-section/index.php
@@ -12,5 +12,5 @@
       <h1 class="bg-cta-section__title"><?php echo get_field("title")?></h1>
     <a class="bg-cta-section__button button button--primary" href="<?php echo get_field("cta")["link"]?>"><?php echo get_field("cta")["title"]?></a>
     <h3 class="bg-cta-section__title"><?php echo get_field("parrafo")?></h3>
-    <h3 class="bg-cta-section__title">ccc</h3>
+   
 </section>
