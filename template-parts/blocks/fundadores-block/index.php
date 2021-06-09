285e2c74965bce4374497c3b56ff173968621bb3
roberto.valdes <roberto.valdes@estudiantes.upr.edu.cu>
2021-06-09 17:45:56 -0400

    Arreglando texto de bloque instagram

--- template-parts/blocks/fundadores-block/index.php
+++ template-parts/blocks/fundadores-block/index.php
@@ -1,7 +1,7 @@
 <section class="founders-section"><img class="founders-section__img" src="<?php echo get_field("img_fondo") ?>">
       <div class="grid founders-section__content">
         <div class="col-start-4 col-width-6 tablet-land-col-start-3 tablet-land-col-width-8 tablet-col-start-2 tablet-col-width-10 mobile-col-start-1 mobile-col-width-12">
-          <h2 class="founders-section__title"><?php echo get_field("titulo")?></h2>
+          <h2 class="founders-section__title">&quot;<?php echo get_field("titulo")?>&quot;</h2>
           <div class="founders-section__description">
           <?php echo get_field("parrafos")?>
           </div>
