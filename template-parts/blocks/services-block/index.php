f4ce6acc853e840d55be5a9bcc0cfc663b7aff65
roberto.valdes <roberto.valdes@estudiantes.upr.edu.cu>
2021-06-09 10:23:42 -0400

    bloque beneficios arreglo

--- template-parts/blocks/services-block/index.php
+++ template-parts/blocks/services-block/index.php
@@ -1,4 +1,4 @@
-<!-- <?php 
+<?php 
 $arrayColStart=[1,4,7,10];
 $arraytablet=[1,7,1,7];
 ?>
@@ -15,36 +15,7 @@ $arraytablet=[1,7,1,7];
           </div>
         </div>
 
-<?php } ?> -->
-<section class="services-section">
-      <div class="grid">
-        <div class="col-start-1 col-width-3 tablet-col-start-1 tablet-col-width-6 mobile-mini-col-start-1 mobile-mini-col-width-12">
-          <div class="service-card"><img class="service-card__img" src="img/service-img1.svg">
-            <div class="service-card__delimiter"></div>
-            <h5 class="service-card__title">free return</h5>
-            <p class="service-card__desc">Todos los pedidos pueden devolverse durante 60 días.</p>
-          </div>
-        </div>
-        <div class="col-start-4 col-width-3 tablet-col-start-7 tablet-col-width-6 mobile-mini-col-start-1 mobile-mini-col-width-12">
-          <div class="service-card"><img class="service-card__img" src="img/service-img2.svg">
-            <div class="service-card__delimiter"></div>
-            <h5 class="service-card__title">made in spain</h5>
-            <p class="service-card__desc">Nuestro producto esta fabricado 100%  en España con las mejores calidades.</p>
-          </div>
-        </div>
-        <div class="col-start-7 col-width-3 tablet-col-start-1 tablet-col-width-6 mobile-mini-col-start-1 mobile-mini-col-width-12">
-          <div class="service-card"><img class="service-card__img" src="img/service-img3.svg">
-            <div class="service-card__delimiter"></div>
-            <h5 class="service-card__title">divide tu pago</h5>
-            <p class="service-card__desc">Los pagos a plazos te permiten repartir los costes a lo largo del tiempo para que puedas comprar todo lo que quieras sin que tu bolsillo</p>
-          </div>
-        </div>
-        <div class="col-start-10 col-width-3 tablet-col-start-7 tablet-col-width-6 mobile-mini-col-start-1 mobile-mini-col-width-12">
-          <div class="service-card"><img class="service-card__img" src="img/service-img4.svg">
-            <div class="service-card__delimiter"></div>
-            <h5 class="service-card__title">garantia</h5>
-            <p class="service-card__desc">En Sporcks queremos garantizar tu satisfacción. Por ello tenemos una solución para todos tus problemas a lo largo de la vida útil de nuestro producto.</p>
-          </div> 
+<?php } ?>
        
       </div>
     </section>
