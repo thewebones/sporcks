e9e72124ff1e507e0ad690875eaf6478c2c20957
alejandro94020423284 <alejandro.pelaez940204@gmail.com>
2021-06-10 13:45:40 -0400

    single

--- template-parts/blocks/lista-atletas/index.php
+++ template-parts/blocks/lista-atletas/index.php
@@ -12,7 +12,8 @@ if($the_query->have_posts() ) :
     while ( $the_query->have_posts() ) :
         $the_query->the_post();
         the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-        echo  '<h1>'.get_post_meta( get_the_ID(), 'edad', true ).'</h1>';
+        $id_perfil=get_post_meta( get_the_ID(), 'perfil', true );
+        echo  '<h1>'.   wp_get_attachment_image_src($id_perfil)[0].'</h1>';
     endwhile;
     wp_reset_postdata();
 else:
---
+++ template-parts/post-template.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pompi
+ * Date: 10/06/21
+ * Time: 13:36
+ */
+?>
+<h1>post</h1>
