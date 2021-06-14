93733bdb72a9fcf10fe78ba88861798c03a67d3f
afelipe94 <afpgilimas@gmail.com>
2021-06-14 11:05:30 -0400

    meta-template

--- single.php
+++ single.php
@@ -20,6 +20,8 @@ get_header();
 			get_template_part( 'template-parts/post-template', get_post_type() );
             if ( 'atletas_post' === get_post_type() )
                 get_template_part( 'template-parts/atletas-template', get_post_type() );
+            if ( 'meta_post' === get_post_type() )
+                get_template_part( 'template-parts/meta-template', get_post_type() );
 
 
 
