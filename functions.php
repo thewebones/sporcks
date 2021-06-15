99c5e857154d096534837669a5d5a1a885ccc42c
roberto.valdes <roberto.valdes@estudiantes.upr.edu.cu>
2021-06-15 12:36:35 -0400

    agregado campos de bloque seccion comparte

--- functions.php
+++ functions.php
@@ -173,7 +173,7 @@ require get_template_directory() . '/inc/customizer.php';
 
 require get_template_directory() . '/inc/register-blocks.php';
 require get_template_directory() . '/inc/acf-cpt.php';
-
+require get_template_directory() . '/inc/bloque-seccion-comparte';
 /**
  * Load Jetpack compatibility file.
  */
