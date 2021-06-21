1155dea60fc33c68e54a2e05feaf23f4d1d49da7
kmilocollado <kmilocollado@gmail.com>
2021-06-21 18:17:19 -0400

    btn add cart

--- template-parts/product-template.php
+++ template-parts/product-template.php
@@ -99,7 +99,22 @@ $product = wc_get_product( get_the_ID() );
               <h3 class="product-info__quantity-heading">Quantity</h3>
               <input class="product-info__quantity-input" type="number" value="1" min="1">
             </div>
-            <button class="product-info__button button button--outline">add to cart</button>
+            <?php
+                            echo apply_filters(
+                                'woocommerce_loop_add_to_cart_link',
+                                sprintf(
+                                    '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" class="button product-info__button button button--outline %s product_type_%s">%s</a>',
+                                    esc_url( $product->add_to_cart_url() ),
+                                    esc_attr( $product->get_id() ),
+                                    esc_attr( $product->get_sku() ),
+                                    
+                                    $product->is_purchasable() ? 'add_to_cart_button' : '',
+                                    esc_attr( $product->product_type ),
+                                    esc_html( $product->add_to_cart_text() )
+                                ),
+                                $product
+                            );?>
+            <!-- <button class="product-info__button button button--outline">add to cart</button> -->
           </form>
           <div class="product-info__content">
 
