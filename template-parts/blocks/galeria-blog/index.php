97f875e99631050d078b4384ec313d42ba95a50a
kmilocollado <kmilocollado@gmail.com>
2021-06-22 11:04:23 -0400

    blog respo img

--- template-parts/blocks/galeria-blog/index.php
+++ template-parts/blocks/galeria-blog/index.php
@@ -35,24 +35,24 @@
     <?php 
        foreach (get_field("galeria") as $item) {
             if($item["ancho"] == "1") { ?>
-        <div class="col-lg-10 col-md-10 col-sm-10 mb-5">
+        <div class="col-lg-10 col-md-10 col-sm-12 mb-5">
         <img src="<?php echo $item["imagen"] ?>" style="height:100% !important; width:100% !important">
         </div>
 
             <?php } 
             else if($item["ancho"] == "2/3") { ?>
-        <div class="col-lg-7 col-md-7 col-sm-10 mb-5">
+        <div class="col-lg-7 col-md-7 col-sm-12 mb-5">
         <img src="<?php echo $item["imagen"] ?>" style="height:100% !important; width:100% !important">
         </div>
 
         <?php } 
             else if($item["ancho"] == "1/2") { ?>
-        <div class="col-lg-5 col-md-5 col-sm-10 mb-5">
+        <div class="col-lg-5 col-md-5 col-sm-12 mb-5">
             <img src="<?php echo $item["imagen"] ?>" style="height:100% !important; width:100% !important;">
         </div>
 
         <?php } else if($item["ancho"] == "1/3") { ?>
-        <div class="col-lg-3 col-md-3 col-sm-10 mb-5">
+        <div class="col-lg-3 col-md-3 col-sm-12 mb-5">
         <img src="<?php echo $item["imagen"] ?>" style="height:100% !important; width:100% !important">
         </div>
         <?php } ?>
