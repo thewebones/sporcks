d8b6bed148645587a201b3df5daca0da25a207c8
kmilocollado <kmilocollado@gmail.com>
2021-06-22 17:13:43 -0400

    form checkout

--- woocommerce/checkout/form-billing.php
+++ woocommerce/checkout/form-billing.php
@@ -33,11 +33,11 @@ defined( 'ABSPATH' ) || exit;
 
 	<div class="woocommerce-billing-fields__field-wrapper">
 		<?php
-		$fields = $checkout->get_checkout_fields( 'billing' );
+			$fields = $checkout->get_checkout_fields( 'billing' );
 
-		foreach ( $fields as $key => $field ) {
-			woocommerce_form_field( $key, $field, $checkout->get_value( $key ) );
-		}
+			foreach ( $fields as $key => $field ) {
+				woocommerce_form_field( $key, $field, $checkout->get_value( $key ) );
+			}
 		?>
 	</div>
 
