7eafe630001a4f08c5958a31d289fbde73419b0b
alejandro94020423284 <alejandro.pelaez940204@gmail.com>
2021-06-30 13:38:56 -0400

    select

--- assets/js/add_ajax.js
+++ assets/js/add_ajax.js
@@ -1,49 +1,25 @@
 /**
  * Created by pompi on 28/06/21.
  */
-// (function ($) {
-//
-//     $(document).on('click', '.add_to_cart_button', function (e) {
-//         e.preventDefault();
-//
-//         var $thisbutton = $(this),
-//             $form = $thisbutton.closest('form.cart'),
-//             id = $thisbutton.val(),
-//             product_qty = $form.find('input[name=quantity]').val() || 1,
-//             product_id = $form.find('input[name=product_id]').val() || id,
-//             variation_id = $form.find('input[name=variation_id]').val() || 0;
-//
-//         var data = {
-//             action: 'woocommerce_ajax_add_to_cart',
-//             product_id: product_id,
-//             product_sku: '',
-//             quantity: product_qty,
-//             variation_id: variation_id,
-//         };
-//
-//         $(document.body).trigger('adding_to_cart', [$thisbutton, data]);
-//
-//         $.ajax({
-//             type: 'post',
-//             url: wc_add_to_cart_params.ajax_url,
-//             data: data,
-//             beforeSend: function (response) {
-//                 $thisbutton.removeClass('added').addClass('loading');
-//             },
-//             complete: function (response) {
-//                 $thisbutton.addClass('added').removeClass('loading');
-//             },
-//             success: function (response) {
-//
-//                 if (response.error && response.product_url) {
-//                     window.location = response.product_url;
-//                     return;
-//                 } else {
-//                     $(document.body).trigger('added_to_cart', [response.fragments, response.cart_hash, $thisbutton]);
-//                 }
-//             },
-//         });
-//
-//         return false;
-//     });
-// })(jQuery);
+(function ($) {
+
+    $(document).on('change', '.product-info__sizes input', function() {
+        $('.product-info__sizes input:checked').each(function(index, element) {
+            var $el = $(element);
+            var thisName = $el.attr('name');
+            var thisVal  = $el.attr('value');
+            $('select[name="'+thisName+'"]').val(thisVal).trigger('change');
+        });
+    });
+    $(document).on('woocommerce_update_variation_values', function() {
+        $('.product-info__sizes input').each(function(index, element) {
+            var $el = $(element);
+            var thisName = $el.attr('name');
+            var thisVal  = $el.attr('value');
+            $el.removeAttr('disabled');
+            if($('select[name="'+thisName+'"] option[value="'+thisVal+'"]').is(':disabled')) {
+                $el.prop('disabled', true);
+            }
+        });
+    });
+})(jQuery);
