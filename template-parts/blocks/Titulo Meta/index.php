dd9e5e8bd6ef45af3aed4d973dffeedffc8031ca
afelipe94 <afpgilimas@gmail.com>
2021-07-08 14:55:49 -0400

    select

--- template-parts/blocks/Titulo Meta/index.php
+++ template-parts/blocks/Titulo Meta/index.php
@@ -7,9 +7,9 @@
  */
 ?>
 
-<div class="metainvisible text-center" style=" padding:100px 0px 100px 0px; background: black; color: white; width: 100%;">
+<div class="metainvisible text-center" style="padding:100px 0px 100px 0px; background: black; color: white; width: 100%;">
     <h1 class="hero-content-section__title mb-5" style="color: white; text-transform: uppercase; margin-top: inherit;"><?php echo get_field("titulo_meta")?></h1>
-    <p class="metainvisiblecont" style="font-family: GTAmericaExpandedRegula; margin-left: auto; margin-right: auto; text-align: center ; width: 560px; line-height: 15px; color:white !important; font-size: 15px;"> <?php echo get_field("texto_meta")?> </p>
+    <p class="metainvisiblecont" style="font-family: GTAmericaExpandedRegular; margin-left: auto; margin-right: auto; text-align: center ; width: 560px; line-height: 15px; color:white !important; font-size: 15px;"> <?php echo get_field("texto_meta")?> </p>
     </div>
 <div style= "background: black;">
 <div style="background: white; height: 1px; width: 92%; margin: auto;" ></div>
@@ -19,7 +19,7 @@
     <section class="textconvict main row  align-items-center" style="width:60% ; margin-left: auto; margin-right:auto;">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 order-2 order-md-1 text-center" style="text-align: left">
             <h1 class="hero-content-section__title mb-5" style="color: white; text-transform: uppercase; text-align: left; margin-top: inherit;"><?php echo get_field("convictory_meta")?></h1>
-            <h2 style="font-size: 22px; width: 180px; font-weight:900; text-align:left; margin-bottom: 50px; text-transform: uppercase; "><?php echo get_field("edicion_especial")?></h2>
+            <h2 style="font-size: 22px; width: 38%; font-weight:900; text-align:left; margin-bottom: 50px; text-transform: uppercase; "><?php echo get_field("edicion_especial")?></h2>
             <p class= "mb-5" style="font-family: GTAmericaExpandedRegular; font-size: 15px; color:white !important; text-align: left; line-height: 15px;"><?php echo get_field("edicion_texto")?></p>
             <p style="font-family: GTAmericaExpandedRegular; margin-bottom: 50px; font-size: 15px; color:white !important; text-align: left; line-height: 15px;"><?php echo get_field("edicion_texto2")?></p>
             <div style="text-align: left; margin-bottom: 20px">
