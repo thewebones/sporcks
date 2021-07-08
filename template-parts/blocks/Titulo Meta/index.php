87769b0699b47db8b243252dbe7e99045ae33003
afelipe94 <afpgilimas@gmail.com>
2021-07-08 15:52:16 -0400

    main css

--- template-parts/blocks/Titulo Meta/index.php
+++ template-parts/blocks/Titulo Meta/index.php
@@ -19,7 +19,7 @@
     <section class="textconvict main row  align-items-center" style="width:60% ; margin-left: auto; margin-right:auto;">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 order-2 order-md-1 text-center" style="text-align: left">
             <h1 class="hero-content-section__title mb-5" style="color: white; text-transform: uppercase; text-align: left; margin-top: inherit;"><?php echo get_field("convictory_meta")?></h1>
-            <h2 style="font-size: 22px; width: 38%; font-weight:900; text-align:left; margin-bottom: 50px; text-transform: uppercase; "><?php echo get_field("edicion_especial")?></h2>
+            <h2 style="font-size: 22px; width: 185px; font-weight:900; text-align:left; margin-bottom: 50px; text-transform: uppercase; "><?php echo get_field("edicion_especial")?></h2>
             <p class= "mb-5" style="font-family: GTAmericaExpandedRegular; font-size: 15px; color:white !important; text-align: left; line-height: 15px;"><?php echo get_field("edicion_texto")?></p>
             <p style="font-family: GTAmericaExpandedRegular; margin-bottom: 50px; font-size: 15px; color:white !important; text-align: left; line-height: 15px;"><?php echo get_field("edicion_texto2")?></p>
             <div style="text-align: left; margin-bottom: 20px">
